5f6783850445fc35cced46d2f18966c2f347f846
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-12 16:04:29 +0200

    Excercise #3

--- index.js
+++ index.js
@@ -11,4 +11,9 @@ let city = "12055 Berlin";
 let phone = "030 991918282";
 console.log(firstname, lastname, address, city, phone);
 
-
+// #3
+let name3 = "John Smith";
+let age3 = "43";
+let profession3 = "teacher";
+let city3 = "Berlin";
+console.log(name3 + " is a " + age3 + " year old " + profession3 + " who lives in " + city3)

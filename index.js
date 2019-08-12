6330b72e09647364f8f691856bc54c05a9a8dd8a
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-12 16:09:46 +0200

    Exercise #4

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let y = "Herrmann";
 console.log(x, y);
 
 // #2
-let firstname = "Horst";
-let lastname = "Schlemmer";
-let address = "Horststrasse 10";
-let city = "12055 Berlin";
-let phone = "030 991918282";
-console.log(firstname, lastname, address, city, phone);
+let firstname2 = "Horst";
+let lastname2 = "Schlemmer";
+let address2 = "Horststrasse 10";
+let city2 = "12055 Berlin";
+let phone2 = "030 991918282";
+console.log(firstname2, lastname2, address2, city2, phone2);
 
 // #3
 let name3 = "John Smith";
@@ -17,3 +17,10 @@ let age3 = "43";
 let profession3 = "teacher";
 let city3 = "Berlin";
 console.log(name3 + " is a " + age3 + " year old " + profession3 + " who lives in " + city3)
+
+// #4
+let length = 16.78; // number/float
+let lastName = "Johnson"; // string/text
+let car = ""; // string
+let isOpen = true; // boolean
+console.log(typeof length, typeof lastName, typeof car, typeof isOpen);

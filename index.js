ccaed574d8d746d26473e4bca22fd5ef1acd3d92
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-12 15:59:12 +0200

    Lesson #2

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-var x = "Manuel";
-var y = "Herrmann";
+// #1
+let x = "Manuel";
+let y = "Herrmann";
 console.log(x, y);
+
+// #2
+let firstname = "Horst";
+let lastname = "Schlemmer";
+let address = "Horststrasse 10";
+let city = "12055 Berlin";
+let phone = "030 991918282";
+console.log(firstname, lastname, address, city, phone);
+
+

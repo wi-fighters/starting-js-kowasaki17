84dd38a9dc7338eee41c5f865662081aa37059bd
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-13 14:55:44 +0200

    #2.2.2

--- Arithmetic/index.js
+++ Arithmetic/index.js
@@ -1,11 +1,12 @@
 // #1
 console.log(17 / 30 * 100 + "%");
 
-// #2
+// #2.1
 let average = (8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4) / 10;
 console.log(average);
 
-// #3
-let average2 = (75 + 70 + 85 + 90 + 100) / 5;
-let average3 = (85 - average2) + average2;
-console.log(average3);
+// #2.2
+let average2 = (85 * 6) - (75 + 70 + 85 + 90 + 100);
+console.log("Score in the sixth test: " + average2);
+
+// #4

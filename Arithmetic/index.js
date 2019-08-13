8905c6b18605b3065c1c34e06e11e089d10cb29c
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-13 14:26:18 +0200

    #2.2

--- Arithmetic/index.js
+++ Arithmetic/index.js
@@ -4,3 +4,8 @@ console.log(17 / 30 * 100 + "%");
 // #2
 let average = (8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4) / 10;
 console.log(average);
+
+// #3
+let average2 = (75 + 70 + 85 + 90 + 100) / 5;
+let average3 = (85 - average2) + average2;
+console.log(average3);

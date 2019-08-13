b21845d43559f3e3d7e996caa52a56df295daade
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-13 15:29:01 +0200

    #5

--- Arithmetic/index.js
+++ Arithmetic/index.js
@@ -7,8 +7,17 @@ console.log(average);
 
 // #2.2
 let average2 = (85 * 6) - (75 + 70 + 85 + 90 + 100);
-console.log("Score in the sixth test: " + average2);
+console.log(`Score in the sixth test: ${average2}`);
 
 // #4
 let average3 = (9*80) - (8*78);
-console.log("James needs a minimum of " + average3 + "% to get an 80% average");
+console.log(`James needs a minimum of ${average3}% to get an 80% average`);
+
+// #5
+let tip1 = 22.35 * 0.1;
+let tip2 = 26.67 * 0.15; 
+let tip3 = 35.92 * 0.2;
+let total1 = 22.35 + tip1;
+let total2 = 26.67 + tip2;
+let total3 = 35.92 + tip3;
+console.log(`Bill1: €${total1} | Bill2: €${total2} | Bill3: €${total3}`);

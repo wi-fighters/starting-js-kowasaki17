11d60d7c36ac1a0dfd9a73018fb4e848c0ab8b3a
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-13 15:09:48 +0200

    #4

--- Arithmetic/index.js
+++ Arithmetic/index.js
@@ -10,3 +10,5 @@ let average2 = (85 * 6) - (75 + 70 + 85 + 90 + 100);
 console.log("Score in the sixth test: " + average2);
 
 // #4
+let average3 = (9*80) - (8*78);
+console.log("James needs a minimum of " + average3 + "% to get an 80% average");

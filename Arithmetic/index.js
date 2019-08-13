adec6ebe687d9c0dc306fc7394e1d91b512589bb
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-13 15:52:37 +0200

    #7-Done

--- Arithmetic/index.js
+++ Arithmetic/index.js
@@ -21,3 +21,27 @@ let total1 = 22.35 + tip1;
 let total2 = 26.67 + tip2;
 let total3 = 35.92 + tip3;
 console.log(`Bill1: €${total1} | Bill2: €${total2} | Bill3: €${total3}`);
+
+// #6
+// Perimeter Square
+let perim = (4*4.75);
+console.log(`${perim}cm`);
+
+// Perimeter Triangel
+let perim2 = (5 + 6 + 7);
+console.log(`${perim2}cm`);
+
+// Square Area
+let area3 = (5*5);
+console.log(`${area3}cm²`);
+
+// Volume Cube
+let volume = (9*9*9);
+console.log(`${volume}cm³`);
+
+// 7
+let tickets = (6450 / 15);
+console.log(`${tickets} Tickets sold`);
+
+let salary = (500 * 52);
+console.log(`She earns $${salary} per year`);

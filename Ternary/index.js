c568ceded02c2c06a2ada8270e58152d513f898d
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-14 09:59:42 +0200

    #2 ternary

--- Ternary/index.js
+++ Ternary/index.js
@@ -3,3 +3,9 @@ const legalAge = 18;
 const exampleAge = 34;
 const canDrive = (exampleAge >= legalAge) ? "Yes, you can drive" : "Nope, you're too young";
 console.log(canDrive);
+
+// #2
+const legalSpeed = 130;
+const exampleSpeed = 120;
+const speedCheck = (exampleSpeed > legalSpeed) ? "You're going too fast - slow down!" : "You're driving below the speed limit.";
+console.log(speedCheck);

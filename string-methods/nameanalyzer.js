74d0a6647f813c70df5bc85d10f03a69e5945719
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-15 09:27:53 +0200

    Nameanalyzer #1

---
+++ string-methods/nameanalyzer.js
@@ -0,0 +1,6 @@
+const name = "Winston";
+const fun ="schmoo";
+console.log(`Hello ${name}.
+Your name is ${name.length} letters long.
+The first letter of your name is ${name [0]}, the last is ${name [6]}.
+If I wanted to be funny, I would call you ${name.substring(0, 3)}${fun}.`)

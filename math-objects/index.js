f9da2d92a21a7ca92213fba5262ef6cd4ef065f8
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-19 15:25:00 +0200

    #4

--- math-objects/index.js
+++ math-objects/index.js
@@ -11,9 +11,12 @@ console.log(Math.ceil (76788.7));
 console.log(Math.ceil(-9.78)); 
 console.log(Math.ceil (43.342));
 
-// #4
+// #3.2
 console.log(Math.floor(3321.32321)); 
 console.log(Math.floor(326.76)); 
 console.log(Math.floor (76788.7)); 
 console.log(Math.floor(-9.78)); 
 console.log(Math.floor (28.329));
+
+// #4
+console.log((Math.floor(Math.random()*6+1)))

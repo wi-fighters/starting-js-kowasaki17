6cb1eb27c1481dd6e2991143e0cb1c843d36e349
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-19 15:14:57 +0200

    #3

--- math-objects/index.js
+++ math-objects/index.js
@@ -3,3 +3,10 @@ console.log(Math.min(10,20,30,40,50));
 
 // #2
 console.log(Math.max(10,20,30,40,50));
+
+// #3
+console.log(Math.ceil(3321.32321)); 
+console.log(Math.ceil(326.76)); 
+console.log(Math.ceil (76788.7)); 
+console.log(Math.ceil(-9.78)); 
+console.log(Math.ceil (43.342));

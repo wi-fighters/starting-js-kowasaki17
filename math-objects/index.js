1679eee8a1a2a71620c70e89f855abd1b3267e5c
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-19 15:17:01 +0200

    #3.2

--- math-objects/index.js
+++ math-objects/index.js
@@ -10,3 +10,10 @@ console.log(Math.ceil(326.76));
 console.log(Math.ceil (76788.7)); 
 console.log(Math.ceil(-9.78)); 
 console.log(Math.ceil (43.342));
+
+// #4
+console.log(Math.floor(3321.32321)); 
+console.log(Math.floor(326.76)); 
+console.log(Math.floor (76788.7)); 
+console.log(Math.floor(-9.78)); 
+console.log(Math.floor (28.329));

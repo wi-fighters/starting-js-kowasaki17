5428bd058f5bac9b47b48c1659aef6e473eb92d1
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-19 15:09:00 +0200

    #2

--- math-objects/index.js
+++ math-objects/index.js
@@ -1,2 +1,5 @@
 // #1
 console.log(Math.min(10,20,30,40,50));
+
+// #2
+console.log(Math.max(10,20,30,40,50));
--- string-methods/nameanalyzer.js
+++ string-methods/nameanalyzer.js
@@ -1,6 +1,7 @@
-const name = "Winston";
+const name = "Winston Churchill";
 const fun ="schmoo";
+let spaceCount = (name.split(" ").length - 1);
 console.log(`Hello ${name}.
-Your name is ${name.length} letters long.
+Your name is ${name.length - spaceCount} letters long.
 The first letter of your name is ${name [0]}, the last is ${name [6]}.
 If I wanted to be funny, I would call you ${name.substring(0, 3)}${fun}.`)

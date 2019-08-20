eedda05b63daaf5765336586e99cae84f8e1e018
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-08-20 10:30:05 +0200

    fin

--- math-objects/index.js
+++ math-objects/index.js
@@ -4,19 +4,19 @@ console.log(Math.min(10,20,30,40,50));
 // #2
 console.log(Math.max(10,20,30,40,50));
 
-// #3
+// #3a
 console.log(Math.ceil(3321.32321)); 
 console.log(Math.ceil(326.76)); 
-console.log(Math.ceil (76788.7)); 
+console.log(Math.ceil(76788.7)); 
 console.log(Math.ceil(-9.78)); 
-console.log(Math.ceil (43.342));
+console.log(Math.ceil(43.342));
 
 // #3.2
 console.log(Math.floor(3321.32321)); 
 console.log(Math.floor(326.76)); 
-console.log(Math.floor (76788.7)); 
+console.log(Math.floor(76788.7)); 
 console.log(Math.floor(-9.78)); 
-console.log(Math.floor (28.329));
+console.log(Math.floor(28.329));
 
 // #4
 console.log((Math.floor(Math.random()*6+1)))
